6b7e5b7fe4d6747b1c4df9f61a3b481459e67501
Dagore <david.gomez.redondo.dev@gmail.com>
2022-10-12 08:50:57 +0200

    templates

---
+++ exercise01/index.php
@@ -0,0 +1,5 @@
+<?php require_once('../templates/header.php'); print_header('Exercise 01') ?>
+
+
+
+<?php require_once('../templates/footer.html'); ?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require_once('templates/header.php'); print_header('UT02-1 Exercises') ?>
+
+<nav>
+  <ul>
+    <li><a href="exercise01">Exercise 01</a></li>
+  </ul>
+</nav>
+
+<?php require_once('templates/footer.html'); ?>
